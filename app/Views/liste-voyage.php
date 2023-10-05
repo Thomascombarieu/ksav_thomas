0e7e60ba7cf8817902f2fe1ada5c11aeecb3ab70
Thomas <combarieu.t@gmail.com>
2023-10-05 12:01:15 +0200

    LISTE voyage css+html

--- app/Views/liste-voyage.php
+++ app/Views/liste-voyage.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>liste-voyages</title>
+    <link href="/inc/liste-voyage.css" type="text/css" rel="stylesheet">
 </head>
 
 
