7ce4fd997b5c34e386f78e124d1d960bb9568347
Thomas <combarieu.t@gmail.com>
2023-10-12 11:47:31 +0200

    layout

---
+++ app/Views/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>liste-voyages</title>
+    <link href="/inc/liste-voyage.css" type="text/css" rel="stylesheet">
+</head>
+
+
+<body>
+
+<?= $this->renderSection('contenue')?>
+
+</body>
+
+</html>
