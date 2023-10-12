6a6702db36e173a83aeb74479f29f0f948695ade
Thomas <combarieu.t@gmail.com>
2023-10-12 11:15:57 +0200

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,26 +8,26 @@ use CodeIgniter\Router\RouteCollection;
 
 //Routes pour la page login
 
-$routes->get('/','Login::index',['as'=> 'Login_Form']);
-$routes->post('/sign-in','Login::signIn',['as'=> 'Login']);
+$routes->get('/', 'Login::index', ['as' => 'Login_Form']);
+$routes->post('/sign-in', 'Login::signIn', ['as' => 'Login']);
 
 //routes pour la page ListeVoyage
 
-$routes->get('liste-voyage','ListeVoyage::index');
-$routes->get('liste-voyage/ajouter','ListeVoyage::ajouterForm',['as'=> 'Ajout_Voyage_Form']);
-$routes->post('liste-voyage/ajouter','ListeVoyage::ajouter',['as'=> 'Ajout_Voyage']);
-$routes->get('liste-voyage/modifier','ListeVoyage:modifierForm',['as'=> 'Modifier_Voyage_Form']);
-$routes->post('liste-voyage/modifier','ListeVoyage:modifier',['as'=> 'Modifier_Voyage']);
+$routes->get('liste-voyage', 'ListeVoyage::index');
+$routes->get('liste-voyage/ajouter', 'ListeVoyage::ajouterForm', ['as' => 'Ajout_Voyage_Form']);
+$routes->post('liste-voyage/ajouter', 'ListeVoyage::ajouter', ['as' => 'Ajout_Voyage']);
+$routes->get('liste-voyage/modifier', 'ListeVoyage:modifierForm', ['as' => 'Modifier_Voyage_Form']);
+$routes->post('liste-voyage/modifier', 'ListeVoyage:modifier', ['as' => 'Modifier_Voyage']);
 
 //route de la page retourClient
 
-$routes->get('liste-retour-client','ListeRetourClient::index',['as'=> 'Liste_Retour_Client']);
+$routes->get('liste-retour-client', 'ListeRetourClient::index', ['as' => 'Liste_Retour_Client']);
 
 //route de la page ListeClient 
 
-$routes->get('liste-client','ListeClient::index',['as'=> 'Liste_Client']);
-$routes->get('liste-client/ajout-client','listeClient::ajouterClientForm',['as'=> 'Ajouter_Client_Form']);
-$routes->post('liste-client/ajout-client','listeClient::ajouterClient',['as'=> 'Ajouter_Client']);
+$routes->get('liste-client', 'ListeClient::index', ['as' => 'Liste_Client']);
+$routes->get('liste-client/ajout-client', 'listeClient::ajouterClientForm', ['as' => 'Ajouter_Client_Form']);
+$routes->post('liste-client/ajout-client', 'listeClient::ajouterClient', ['as' => 'Ajouter_Client']);
 
 //route de la page ajout et modification voyage (déja présente sur liste voyage mais a vérifier)
 
@@ -40,11 +40,9 @@ $routes->post('liste-client/ajout-client','listeClient::ajouterClient',['as'=> '
 //route pour la page ajout client 
 
 //$routes->get('ajouter-retour-client-voyage','AjouterRetourClientVoyage::index');
-$routes->get('ajouter-retour-client-voyage','AjouterRetourClientVoyage::ajouterForm',['as'=> 'Ajout_Retour_Client_Form']);
-$routes->post('ajouter-retour-client-voyage','AjouterRetourClientVoyage::ajouter',['as'=> 'Ajout_Retour_Client']);
+$routes->get('ajouter-retour-client-voyage', 'AjouterRetourClientVoyage::ajouterForm', ['as' => 'Ajout_Retour_Client_Form']);
+$routes->post('ajouter-retour-client-voyage', 'AjouterRetourClientVoyage::ajouter', ['as' => 'Ajout_Retour_Client']);
 
 //route de base (page index)
-$routes->get('menu','Menu::index');
-//Menu Page
 
 //$routes->get('/', 'Home::index');
