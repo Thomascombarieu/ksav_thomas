e2e0b24aac1f9e53dab3d92e6835aeced630ee80
Mathyss <mathyssvotte@gmail.com>
2023-10-12 09:49:00 +0200

    modification (ajout voyage formulaire fini)

--- app/Views/liste-voyage/ajouter.php
+++ app/Views/liste-voyage/ajouter.php
@@ -9,18 +9,188 @@
 
 <body>
     <?php
+    //import du Form Helper
     helper('form');
-    $attributs = ['classe' => 'maclasse', 'id' => 'monid'];
-    echo form_open('my-route', $attributs);
-    $data = [
-        'name'      => 'username',
-        'id'        => 'username',
-        'value'     => 'johndoe',
-        'maxlength' => '100',
+    //Création du formulaire AjoutVoyage
+    echo form_open('ajoutVoyage');
+    form_reset('Vider');
+    
+    // Input pour le nom du voyage
+    
+    echo form_label('Nom du Voyage ');
+    $nomVoyageForm = [
+        'name'      => 'nomVoyage',
+        'id'        => 'nomVoyage',
+        'placeholder'=> 'Entrez le nom du voyage',
+        //'value'     => set_value('johndoe'),
+        //'maxlength' => '100',
         'size'      => '50',
-        'style'     => 'width:50%',
+        'style'     => 'width:30%',
     ];
-    echo form_input($data);
+    echo form_input($nomVoyageForm);
+
+    //input textarea pour la description voyage
+
+    echo form_label('Description du voyage ');
+    $descVoyageForm = [
+        'name'      => 'descVoyage',
+        'id'        => 'descVoyage',
+        'placeholder'=> 'Entrez la description du voyage',
+        //'value'     => set_value('johndoe'),
+        //'maxlength' => '100',
+        'size'      => '50',
+        'style'     => 'width:30%',
+    ];
+    echo form_textarea($descVoyageForm);
+
+    //input select pour type Voyage
+
+    echo form_label('Type de voyage ');
+    $options = [
+        'circuit'  => 'Circuit',
+        'croisiere'    => 'Croisière',
+        'sejour'  => 'Séjour',
+        'vol' => 'Vol',
+    ];
+    
+    echo form_dropdown('typeVoyage', $options,);
+
+    //input destination voyage
+
+    echo form_label('Destination du voyage');
+    $destinationVoyageForm = [
+        'name'      => 'destVoyage',
+        'id'        => 'destVoyage',
+        'placeholder'=> 'Entrez la destination du voyage',
+        //'value'     => set_value('johndoe'),
+        //'maxlength' => '100',
+        'size'      => '50',
+        'style'     => 'width:30%',
+    ];
+    echo form_input($destinationVoyageForm);
+
+    //checkbox pour les prestations (il y en a plusieurs)
+    echo form_label('Transfert');
+    $transfertForm = [
+        'name'    => 'transfert',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($transfertForm);
+
+    //hotel
+    echo form_label('Hôtel');
+    $hotelForm = [
+        'name'    => 'hotel',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($hotelForm);
+
+    //Restauration
+    echo form_label('Restauration');
+    $restaurationForm = [
+        'name'    => 'restauration',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($restaurationForm);
+
+    //Service & acceuil
+    echo form_label('Service & Accueil');
+    $servForm = [
+        'name'    => 'serv',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($servForm);
+
+    //animation
+    echo form_label('Animation');
+    $animationForm = [
+        'name'    => 'animation',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($animationForm);
+
+    //excursion & guide
+    echo form_label('Excursion & guide');
+    $guideForm = [
+        'name'    => 'guide',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($guideForm);
+
+    //Transport aérien
+    echo form_label('Transport Aérien');
+    $avionForm = [
+        'name'    => 'avion',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($avionForm);
+
+    //Transport car
+    echo form_label('Transport Car');
+    $carForm = [
+        'name'    => 'car',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($carForm);
+
+    //Thalasso & spa 
+    echo form_label('Thalasso & SPA');
+    $spaForm = [
+        'name'    => 'spa',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($spaForm);
+
+    //croisière
+    echo form_label('Croisière');
+    $croisiereForm = [
+        'name'    => 'croisiere',
+        'id'      => 'prestation',
+        //'value'   => 'accept',
+        //'checked' => true,
+        'style'   => 'margin:10px',
+    ];
+    
+    echo form_checkbox($croisiereForm);
+
+    form_submit('Valider');
+
     ?>
 </body>
 
