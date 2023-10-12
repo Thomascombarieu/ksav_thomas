f2e5e659846198f4810c3e973422eb45c29e5666
Thomas <combarieu.t@gmail.com>
2023-10-12 10:56:15 +0200

    modif

---
+++ app/Controllers/Menu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Menu extends BaseController
+{
+    public function index(): string
+    {
+        return view('menu');
+    }
+
+    
+}
--- app/Views/liste-voyage.php
+++ app/Views/liste-voyage.php
@@ -11,8 +11,8 @@
 
 <body>
 
-    <a href="<?= url_to('Ajout_Voyage_Form') ?>"> + </a>
-    <a href="<?= url_to('Modifier_Voyage_Form') ?>"> / </a>
+    <a href="<?= url_to('Ajout_Voyage_Form') ?>"> + </a><br><br>
+    <a href="<?= url_to('Modifier_Voyage_Form') ?>"> / </a><br><br>
 
     <?php
 
