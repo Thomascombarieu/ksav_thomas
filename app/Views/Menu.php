4b27dd213a7aa679a298b5fe1a0eaf70853864dc
Thomas <combarieu.t@gmail.com>
2023-10-12 08:34:53 +0200

    n

---
+++ app/Views/Menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>liste-voyages</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+
+
+<ul class="menu">
+	<li><a href="Ajout-Modif-Voyage.html" target="_top">Ajout/Modif Voyage</a></li>
+	<li class="deroulant"><a href="animaux.html"target="_top">animaux</a></li>		
+	<li><a href="Incription.html"target="_top">Inscription</a></li>
+		
+</ul>
+
+
+</html>
+
+
+
--- app/Views/liste-voyage.php
+++ app/Views/liste-voyage.php
@@ -29,3 +29,5 @@ echo $table->generate();
 </body>
 
 </html>
+
+
