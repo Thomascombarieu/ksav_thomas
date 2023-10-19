da02dc780d1955418c57baee06b07a189881f7f0
Thomas <combarieu.t@gmail.com>
2023-10-19 09:21:31 +0200

    c

--- app/Views/liste-client.php
+++ app/Views/liste-client.php
@@ -7,8 +7,8 @@ $table = new \CodeIgniter\View\Table();
     $table->setHeading('N°Client', 'Nom', 'Prénom', 'Adresse','Tel');
     $table->addRow('1','Votte','Mathyss','mathyssvotte@gmail.com','07 83 54 72 55' );
     $table->addRow('124','Combarieu','Thomas','Combarieu.tho82@gmail.com','06 77 88 44 22' );
-    $table->addRow('125',);
-    $table->addRow('315',);
+    $table->addRow('125','Alim','Miloud','Miloud@Hotmail.fr','06 99 34 58 02');
+    $table->addRow('315','Dumas','Jade','IJade@gmail.com','07 81 02 58 21');
 
     echo $table->generate();
 ?>
