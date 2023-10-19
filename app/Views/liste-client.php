2d2a0b8f32041a199ff6764816b39555aaa39607
Thomas <combarieu.t@gmail.com>
2023-10-19 09:14:46 +0200

    maj contante

--- app/Views/liste-client.php
+++ app/Views/liste-client.php
@@ -5,5 +5,11 @@ $table = new \CodeIgniter\View\Table();
     // $php_Button = new button;
 
     $table->setHeading('N°Client', 'Nom', 'Prénom', 'Adresse','Tel');
+    $table->addRow('1','Votte','Mathyss','mathyssvotte@gmail.com','07 83 54 72 55' );
+    $table->addRow('124','Combarieu','Thomas','Combarieu.tho82@gmail.com','06 77 88 44 22' );
+    $table->addRow('125',);
+    $table->addRow('315',);
+
+    echo $table->generate();
 ?>
 <?= $this->endSection()?>
