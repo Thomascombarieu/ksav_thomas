e5f56ee1620f4be7bc43e76dda1416142039b804
Mathyss <mathyssvotte@gmail.com>
2023-10-19 10:52:53 +0200

    mise a jour CSS

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=stylesheet href=Ksav.css>
+    <link rel=stylesheet href=inc/Ksav.css>
     <title>Layout</title>
 </head>
 <header>
