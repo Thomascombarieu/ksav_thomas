0a6db2407acf7c2f5315794e4839a85da5902aba
Mathyss <mathyssvotte@gmail.com>
2023-11-16 11:48:11 +0100

    souci css réglé

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,5 +1,5 @@
 <?php
-
+helper('html');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -7,16 +7,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=stylesheet href=inc/Ksav.css>
+    <?php echo link_tag('inc/Ksav.css') ?>
     <title>KSAV</title>
 </head>
 <header>
     <nav>
         <ul>
 
-            <li><a href="liste-voyage">Liste Voyage</a></li>
-            <li><a href="liste-client">Liste clients</a></li>
-            <li><a href="liste-retour-client">Liste retour clients</a></li>
+            <li><a href="<?= url_to('Liste_Voyage') ?>">Liste Voyage</a></li>
+            <li><a href="<?= url_to('Liste_Client') ?>">Liste clients</a></li>
+            <li><a href="<?= url_to('Liste_Retour_Client') ?>">Liste retour clients</a></li>
             <li><a href="<?= url_to('Ajouter_Client_Form') ?>">Ajouter un Client</a></li>
             <li><a href="<?= url_to('Ajout_Voyage_Form') ?>">Ajouter un voyage</a></li>
             
