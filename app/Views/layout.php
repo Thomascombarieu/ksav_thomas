93bad6864163d7e30fb1c211ff3f3f461309d46e
Thomas <combarieu.t@gmail.com>
2023-11-16 10:21:25 +0100

    layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -17,6 +17,8 @@
             <li><a href="liste-voyage">Liste Voyage</a></li>
             <li><a href="liste-client">Liste clients</a></li>
             <li><a href="main.html">Liste retour clients</a></li>
+            <li><a href="main.html">Ajout voyage</a></li>
+            <li><a href="main.html">Ajout client</a></li>
             
             
         </ul>
