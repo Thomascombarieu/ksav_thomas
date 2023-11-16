4ef5ae1f163efb445c80ac0fd09fa5aaeaaab5a4
Thomas <combarieu.t@gmail.com>
2023-11-16 08:48:11 +0100

    voyages

--- app/Models/Voyages.php
+++ app/Models/Voyages.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class Voyage extends Model
+class Voyages extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'voyage';
